db18e92c7ff9a634d9e5ec089e706db379d23d1c
azufb <curiousazunas@gmail.com>
2025-08-14 10:22:42 +0900

    一旦commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,27 @@
+import { ApolloServer } from "@apollo/server";
 import express, { Request, Response } from "express";
+import { buildSchema } from "graphql";
+import { createHandler } from "graphql-http";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+const schema = buildSchema(`type Query { hello: String } `);
+
+const resolver = {
+  hello() {
+    return "Hello world!";
+  },
+};
+
+app.use(
+  "/graphql",
+  createHandler({
+    schema,
+    rootValue: resolver,
+  })
+);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on http://localhost:3000/`);
 });
