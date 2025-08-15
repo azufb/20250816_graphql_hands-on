f54700b77eaf87f907d377adbad00ae272e8206c
azufb <curiousazunas@gmail.com>
2025-08-15 12:46:11 +0900

    ページ遷移まで実装

--- frontend/src/features/members/memberList/MemberListContainer.tsx
+++ frontend/src/features/members/memberList/MemberListContainer.tsx
@@ -1,16 +1,26 @@
+import { useNavigate } from "react-router";
 import { useFetchMemberListQuery } from "../../../gql/generated/graphql";
 import { MemberListPresenter } from "./MemberListPresenter";
 
 export const MemberListContainer = () => {
+  const navigate = useNavigate();
   const { data, error } = useFetchMemberListQuery();
 
   if (error) {
     throw error.message;
   }
 
+  /**
+   * メンバー詳細画面に遷移
+   */
+  const handleClickRow = (id: string) => {
+    navigate(`/members/${id}`);
+  };
+
   return (
-    <>
-      <MemberListPresenter memberList={data?.fetchMemberList ?? []} />
-    </>
+    <MemberListPresenter
+      memberList={data?.fetchMemberList ?? []}
+      onClickRow={handleClickRow}
+    />
   );
 };
--- frontend/src/features/members/memberList/MemberListPresenter.tsx
+++ frontend/src/features/members/memberList/MemberListPresenter.tsx
@@ -2,25 +2,32 @@ import type { FetchMemberListQuery } from "../../../gql/generated/graphql";
 
 interface MemberListPresenterProps {
   memberList: FetchMemberListQuery["fetchMemberList"];
+  onClickRow: (id: string) => void;
 }
 
 export const MemberListPresenter = ({
   memberList,
+  onClickRow,
 }: MemberListPresenterProps) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>名前</th>
-        </tr>
-      </thead>
-      <tbody>
-        {memberList.map((member) => (
+    <>
+      <h2>一覧</h2>
+      <table>
+        <thead>
           <tr>
-            <td>{member.name}</td>
+            <th>名前</th>
+            <th />
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {memberList.map((member, index) => (
+            <tr key={member.id} onClick={() => onClickRow(member.id)}>
+              <td>{index + 1}</td>
+              <td>{member.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
